4561b780f73ecdbd770d83fe1893257ecc205e47
roarke.gaskill@gmail.com <roarke.gaskill@gmail.com>
2012-02-10 22:37:10 -0600

    Some UI work

--- EShift-ui/src/main/java/com/askill/eshift/ui/main/MainPageController.java
+++ EShift-ui/src/main/java/com/askill/eshift/ui/main/MainPageController.java
@@ -1,6 +1,9 @@
 package com.askill.eshift.ui.main;
 
+import com.askill.eshift.ui.user.UserAdminUI;
 import com.askill.eshift.user.User;
+import com.vaadin.ui.MenuBar;
+import com.vaadin.ui.MenuBar.MenuItem;
 
 public class MainPageController {
 
@@ -10,10 +13,21 @@ public class MainPageController {
 	public MainPageController(User user){
 		model.setUser(user);
 		view.setModel(model);
+		view.setUserAdminMenuCommand(new UserAdminCommand());
 	}
 	
 	public MainPage getView(){
 		return view;
+	}
+	
+	private class UserAdminCommand implements MenuBar.Command {
+
+		@Override
+		public void menuSelected(MenuItem selectedItem) {
+			view.setContent(new UserAdminUI());
+			
+		}
+		
 	}
 	
 }
